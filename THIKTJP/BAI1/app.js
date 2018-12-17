e6fbf4db0d7bf8808319477cb46fe05c601578bb
Tuancodeweb <phantuani.c.t.u@gmail.com>
2018-12-17 11:48:05 +0700

    edit html

--- THIKTJP/BAI1/app.js
+++ THIKTJP/BAI1/app.js
@@ -1,4 +1,4 @@
-// can thiệp vào giá gốc của sản phẩm
+ // can thiệp vào giá gốc của sản phẩm
     var price = document.getElementById("price");
         // can thiệp vào ô xuất dữ liệu ở table
             var sanpham = document.getElementById("sanpham");
@@ -8,8 +8,8 @@
                             var total1 = document.getElementById("total1");
                                 // can thiệp vào nút xóa
                                     var function1 = document.getElementById("function1");
-                                        
-
+                                            var PO = document.getElementById("PO");
+                                              
 
                                     // sự kiện xóa sản phẩm
                                     var function1 = function()
@@ -19,13 +19,16 @@
 
                                     // sư kiện tăng giá trị và tính tổng total
 
+
                                     var changePrice = function()
                                     {
                                         // tăng giá trị và tính tiền
                                        total1.innerText = parseInt(price.innerText) * parseInt(quantity.value);
+                                      
                                        if((quantity.value)>=6)
                                        {
-                                           alert("sản phẩm đã hết");
+                                           alert("số lượng hết hàng");
+                                          
                                            if(sanpham.style.display=="")
                                            {
                                             quantity.value="5";
@@ -47,5 +50,31 @@
                                         {
                                             quantity.value = parseInt(quantity.value)+1;
                                             changePrice();
+                                        }
+                                    }
+
+                                    var buyProduct2 = function()
+                                    {
+                                        if(sanpham.style.display=="none")
+                                        {
+                                            alert("bạn phải chọn số lượng đã");
+                                           
+                                        }
+                                        else if (sanpham.style.display == "")
+                                        {
+                                            
+                                            var PO_DU = PO.innerText;
+                                            var tuan = confirm("Tên sản phẩm:"+PO_DU + "\n" + "số lượng sản phẩm đã mua:" + quantity.value + "\n" + "giá thành sản phẩm:" + price.innerText + "\n" + "tổng giá tiền thanh toán" +"$"+ total1.innerText + "\n" +  "ảnh của sản phẩm \n");
+
+        
+                                            if (tuan == true)
+                                            {
+                                                alert("đã mua thành công");
+                                            }
+                                             else
+                                            {
+                                                alert("đã hủy mua sản phẩm này");
+                                                sanpham.style.display="none";
+                                            }
                                         }
                                     }
